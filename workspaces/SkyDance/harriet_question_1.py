16aacf14171e7ccc9b27a8f42eb84614f5183da1
Harriet <haarrii@Harriets-MacBook-Pro.local>
2021-10-30 14:16:58 +0200

    fixing docstring

--- workspaces/SkyDance/harriet_question_1.py
+++ workspaces/SkyDance/harriet_question_1.py
@@ -6,44 +6,42 @@ import json
 __json_path = './src/sample4.json'
 
 def _get_json_dict(*args, **kwargs):
-    '''
+    """
     Read the json file and convert it to list.
 
-    Returns json data as list.
-    '''
-
+    Returns:
+        dict: json data
+    """
     with open(__json_path) as f:
         data = json.load(f)
 
     return data
 
 def check_characters(input, *args, **kwargs):
-    '''
+    """
     Check if character buffer reads the same backwards as forwars.
 
-    Parameters:
-    input (any type): the characters you want to check.
+    Args:
+        input (any type): the characters you want to check.
 
-    Returns True if it read the same backwards as forwards.
-    Return False if not.
-    '''
+    Returns:
+        bool: Returns True if it read the same backwards as forwards.
+        bool: Return False if not.
+    """
 
     ### check if the input parameter is a string and if it reads the same backwars as forwards ###
-    if input.lower() == input.lower()[::-1]:
+    if str(input).lower() == str(input).lower()[::-1]:
         return True
     else:
         return False
 
 def check_characters_from_json(*args, **kwargs):
-    '''
-    Check if character buffer reads the same backwards as forwars.
+    """
+    Check if character buffer reads the same backwards as forwars from a json file.
 
-    Parameters:
-    input (any type): the characters you want to check.
-
-    Returns a list with True of False if the character buffer reads the same backwards as forwars.
-    '''
-
+    Returns:
+        list: Returns a list with True of False if the character buffer reads the same backwards as forwars.
+    """
     ### get the json data ###
     data = _get_json_dict()
     characters = [people.get('firstName') for people in data.get('people')]
