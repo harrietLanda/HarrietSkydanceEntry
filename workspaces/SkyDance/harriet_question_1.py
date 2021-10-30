0ad2b6570390559d107dc03f38b197322132c9f6
haarriii <ximharri@gmail.com>
2021-10-30 12:55:42 +0200

    first upload

---
+++ workspaces/SkyDance/harriet_question_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+
+def _get_json_dict():
+    '''
+
+    Parameters:
+    argument1 (string): Json file path
+
+    Returns json data as list
+
+    '''
+
+    
