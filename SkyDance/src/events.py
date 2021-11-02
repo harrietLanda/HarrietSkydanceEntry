9cd70854756e95748123e7408a87e7821b7e99ad
Harriet Landa <52937884+haarriii@users.noreply.github.com>
2021-11-02 19:32:39 +0100

    Add files via upload

---
+++ SkyDance/src/events.py
@@ -0,0 +1,9 @@
+### Create a main dictionary to organize the events with the actions ###
+
+EVENTS = {
+        "New Asset": 'new_asset',
+        "Publish Asset" : '...',
+        "Publish Lighting Scene" : '...'
+    }
+
+
