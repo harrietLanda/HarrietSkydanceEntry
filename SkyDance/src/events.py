d2415a4ed2c091a16c96939817132c117c9259f5
Harriet Landa <52937884+haarriii@users.noreply.github.com>
2021-11-02 19:33:35 +0100

    Delete SkyDance directory

--- SkyDance/src/events.py
+++
@@ -1,9 +0,0 @@
-### Create a main dictionary to organize the events with the actions ###
-
-EVENTS = {
-        "New Asset": 'new_asset',
-        "Publish Asset" : '...',
-        "Publish Lighting Scene" : '...'
-    }
-
-
